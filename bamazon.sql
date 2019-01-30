55432077c17e8572adac01a20770cf301afc9955
adamjorycromer <adamjorycromer@gmail.com>
2019-01-29 22:31:42 -0700

    created env and started js and sql files

---
+++ bamazon.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    id INT NOT NULL AUTO INCREMENT,
+    name VARCHAR(35) NOT NULL,
+    department VARCHAR(35) NOT NULL,
+    price FLOAT(10) NOT NULL,
+    quantity FLOAT(10),
+    PRIMARY KEY (id)
+);
+
---
+++ customer.js
@@ -0,0 +1,19 @@
+//Must require certain NPM packages to run
+require("dotenv").config();
+var password = require("./pass.js");
+var mysql = require("mysql");
+
+//Divider to diplayer cleaner code
+var divider = ("\n\n------------------------\n");
+
+//Pass read using dotenv
+var pass = password.pass.id;
+
+//Connection to local mysql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: pass,
+    database: "bamazon"
+});
