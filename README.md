d0b85d022cf04c8c9658283192caf711281a405e
adamjorycromer <adamjorycromer@gmail.com>
2019-03-13 16:06:37 -0600

    fixed readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Store Front
-Amazon Like CLI Storefront
+# <b>CLI Bookstore</b>
+
+A node command line interface which a customer can buy books in the current inventory. A customer can see current inventory, price for each book, and current quantity. A total is displayed total and the current inventory is adjusted. 
+<br>
+
+## <b>How To Use</b>
+
+To run the CLI Bookstore you must run the program customer.js in the command line of node. You will be shown the current inventory and current prices for each book. You then can add each item wanted to purchase to your cart. It will then ask if you would like to continue shopping or checkout.
+
+### <b>Help</b>
+
+Select help when prompted and the console will log information to inform or remind the user of acceptable arguments.
+<br>
+
+## <b>Install This Program</b>
+
+
+Clone this repository.
+
+You must create API Keys from [Spotify,](https://developer.spotify.com/my-applications/#!/) [Bands In Town,](http://www.artists.bandsintown.com/bandsintown-api) and [OMDb.](http://www.omdbapi.com/apikey.aspx)
+
+Create a .env file in this format with your API Keys instead of where it says API Key. Spotify requires an ID key and a secret Key.
+
+Open up your folder container the Storefront files in the command line. **'CTRL + `**<br>
+
+Type **'npm install'**
+
+And then your file should be good to go. You can now type your arguments in to the command line.
+<br><br>
+
+[Made by Adam Cromer, 2019.](http://www.adamcromer.com)
+
--- customer.js
+++ customer.js
@@ -6,70 +6,81 @@ var inquirer = require("inquirer");
 
 //Divider to diplayer cleaner code
 var divider = ("\n\n------------------------\n");
-// var newArr = [];
+var newArr = [];
 
-//Pass read using dotenv
-var pass = password.pass.id;
-
-//Connection to local mysql database
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: pass,
-    database: "bamazon"
-});
-
-connection.connect(function (err) {
+connection.query("SELECT name, author, department, price, quantity FROM products ORDER BY department, author, name", function (err, res) {
     if (err) throw err;
 
-    console.log("connected as id " + connection.threadId + "\n");  
-    // console.log(newArr);
+    var newArr = [];
 
-    startFunc(readInv);
-});
+    // Log all results of the SELECT statement
+    for (var i = 0; i < res.length; i++) {
+        newArr.push(res[i].name + " | Author: " + res[i].author + " | Genre: " + res[i].department + " | Price: " + res[i].price + " | Quantity: " + res[i].quantity);
+    }
 
-function startFunc(arr) {
 
-    inquirer
-        .prompt(
-            {
-                name: "action",
-                type: "list",
-                message: "Welcome to the Command Line Book store. \nHere is our current inventory. \nWhat book would you like to buy?",
-                choices: arr
-            }
-        )
-        .then(function (answer) {
-            console.log(answer.action);
-        });
-}
+    //Pass read using dotenv
+    var pass = password.pass.id;
 
-function qtyFunc() {
-    inquirer
-        .prompt({
-            name: "quantity",
-            type: "input",
-            message: "How many of product would you like to buy?"
-        })
-        .then(function (answer) {
+    //Connection to local mysql database
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: pass,
+        database: "bamazon"
+    });
 
-        });
-}
-
-function readInv() {
-    connection.query("SELECT name, author, department, price, quantity FROM products ORDER BY department, author, name", function (err, res) {
+    connection.connect(function (err) {
         if (err) throw err;
 
-        var newArr = [];
+        console.log("connected as id " + connection.threadId + "\n");
+        // console.log(newArr);
 
-        // Log all results of the SELECT statement
-        for (var i = 0; i < res.length; i++) {
-            newArr.push(res[i].name + " | Author: " + res[i].author + " | Genre: " + res[i].department + " | Price: " + res[i].price + " | Quantity: " + res[i].quantity);
-        }
-
-        return newArr;
-        
-        connection.end();
+        startFunc(readInv);
     });
-}
+
+    function startFunc(arr) {
+
+        inquirer
+            .prompt(
+                {
+                    name: "action",
+                    type: "list",
+                    message: "Welcome to the Command Line Book store. \nHere is our current inventory. \nWhat book would you like to buy?",
+                    choices: arr
+                }
+            )
+            .then(function (answer) {
+                console.log(answer.action);
+            });
+    }
+
+    function qtyFunc() {
+        inquirer
+            .prompt({
+                name: "quantity",
+                type: "input",
+                message: "How many of product would you like to buy?"
+            })
+            .then(function (answer) {
+
+            });
+    }
+
+    function readInv() {
+        connection.query("SELECT name, author, department, price, quantity FROM products ORDER BY department, author, name", function (err, res) {
+            if (err) throw err;
+
+            var newArr = [];
+
+            // Log all results of the SELECT statement
+            for (var i = 0; i < res.length; i++) {
+                newArr.push(res[i].name + " | Author: " + res[i].author + " | Genre: " + res[i].department + " | Price: " + res[i].price + " | Quantity: " + res[i].quantity);
+            }
+
+            return newArr;
+
+            connection.end();
+        });
+    }
